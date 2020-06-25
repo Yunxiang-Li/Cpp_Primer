2161eb1fe2053c1e145996051ff58c552f18bef1
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-25 03:43:20 -0400

    Create 3.5_2.cpp

---
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.5_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using std :: cout;
+using std :: cin;
+using std :: string;
+
+
+int main()
+{
+    string strCct, strEach;
+
+    while (cin >> strEach) {
+
+        strCct += strEach + " ";
+
+        //When we press enter to go to a new line, the input process is over.
+        if (cin.get() == '\n')
+            break;
+    }
+
+    cout << "The concatenated string is " << strCct << '\n';
+
+    return 0;
+}
