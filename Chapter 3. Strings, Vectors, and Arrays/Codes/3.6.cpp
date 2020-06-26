392fc5576f60105bb5e7b7dd5b128470a76c19db
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-26 18:23:47 -0400

    Create 3.6.cpp

---
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std :: cout;
+using std :: cin;
+using std :: string;
+
+
+int main()
+{
+    string str ("This is a simple string");
+
+    for (char &c : str)
+        c = 'X';
+
+    cout << "The string after change is " << str << '\n';
+
+    return 0;
+}
