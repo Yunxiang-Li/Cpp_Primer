47782e175f121e2d44a1ad0271ef68c109e8415f
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-29 19:48:24 -0400

    Update 3.17.cpp

--- Chapter 3. Strings, Vectors, and Arrays/Codes/3.17.cpp
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.17.cpp
@@ -14,17 +14,22 @@ int main()
     cout << "Please input a sequence of strings" << '\n';
 
     while (cin >> myStr) {
+        //When user input "end", break the loop
         if (myStr == "end")
             break;
         else
+            //Push each string to the vector.
             vec.push_back(myStr);
     }
 
-
+    //For each string in the vector
     for (auto& str : vec)
+        //For each char in each string
         for (auto& letter : str)
+            //Change each char to its uppercase form.
             letter = toupper(letter);
 
+    //Print the transformed elements, eight words to a line.
     for (decltype(vec.size()) index = 0; index != vec.size(); index++) {
         if (index % 8 == 0 && index != 0)
             cout << '\n';
