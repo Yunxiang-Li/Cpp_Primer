cef21e9f5620484ceece3b1ee7ed678cbc11cde4
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-28 23:18:09 -0400

    Create 3.19.cpp

---
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+
+int main()
+{
+    vector<int> ivec1(10, 42);
+    
+    vector<int> ivec2{42, 42, 42, 42, 42, 42, 42, 42, 42, 42};
+    
+    vector<int> ivec3;
+    for (int i = 0; i != 10; ++i) ivec3.push_back(42);
+}
