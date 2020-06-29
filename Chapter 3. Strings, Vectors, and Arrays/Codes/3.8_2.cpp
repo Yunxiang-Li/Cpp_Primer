87763cc588041edde60e9e81fd49f90cf89d526a
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-29 19:28:48 -0400

    Update 3.8_2.cpp

--- Chapter 3. Strings, Vectors, and Arrays/Codes/3.8_2.cpp
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.8_2.cpp
@@ -9,9 +9,8 @@ using std :: string;
 int main()
 {
     string str ("This is a simple string");
-
-
-
+    
+    //Change each char of string to 'X'.
     for (decltype(str.size()) index = 0; index < str.size(); index ++)
         str[index] = 'x';
 
