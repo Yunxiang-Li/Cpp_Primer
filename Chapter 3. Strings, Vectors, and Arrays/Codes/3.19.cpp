0be59d52a116f7a24332591cc0982f33a3fe86a6
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-29 19:51:48 -0400

    Update 3.19.cpp

--- Chapter 3. Strings, Vectors, and Arrays/Codes/3.19.cpp
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.19.cpp
@@ -5,10 +5,13 @@ using std::vector;
 // Define a vector and give it ten elements in three ways.
 int main()
 {
+    //Specifying size and initializing all values 
     vector<int> ivec1(10, 42);
     
+    //List initialization.
     vector<int> ivec2{42, 42, 42, 42, 42, 42, 42, 42, 42, 42};
     
+    //For loop initialization.
     vector<int> ivec3;
     for (int i = 0; i != 10; ++i) ivec3.push_back(42);
 }
