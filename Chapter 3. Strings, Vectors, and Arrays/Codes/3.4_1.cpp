aea31112ed2cf6025268d6d6653097c367eea217
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-29 19:14:25 -0400

    Update 3.4_1.cpp

--- Chapter 3. Strings, Vectors, and Arrays/Codes/3.4_1.cpp
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.4_1.cpp
@@ -10,9 +10,12 @@ int main()
 {
     string strA, strB;
 
+    //Input two strings.
     while (cin >> strA >> strB) {
+        //Check if two strings are equal.
         if (strA == strB)
             cout << "Two strings are equal!" << '\n';
+        //Check which string is larger.
         else
             cout << "The larger string is " << (strA < strB ? strB : strA) << '\n';
     }
