11c72f8e782be0a3d98f18eae9ac95f90f526a7a
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-28 23:28:19 -0400

    Create 3.20_1.cpp

---
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.20_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std :: cout;
+using std :: cin;
+using std :: vector;
+
+int main()
+{
+    vector<int> vec;
+    int element;
+    cout << "Please input a set of integers" << '\n';
+
+    while (cin >> element)
+        vec.push_back(element);
+
+    if (vec.empty()) {
+        cout << "No integers input" << '\n';
+        return -1;
+    }
+
+    else if (vec.size() == 1) {
+        cout << "Only one element exists" << '\n';
+        cout << "It is " << vec[0] << '\n';
+    }
+
+    else {
+        for (decltype(vec.size()) i = 0; i < vec.size() - 1; i++) {
+            cout << "Sum of every two adjacent elements is " << vec[i] + vec[i + 1] << '\n';
+        }
+    }
+
+    return 0;
+}
