2e3dd9d96c627135e809860776b1f89ce57d430f
Yunxiang Li <kitzukikokone@gmail.com>
2020-06-29 19:54:12 -0400

    Update 3.20_1.cpp

--- Chapter 3. Strings, Vectors, and Arrays/Codes/3.20_1.cpp
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.20_1.cpp
@@ -15,16 +15,19 @@ int main()
     while (cin >> element)
         vec.push_back(element);
 
+    //Check if the vector is empty.
     if (vec.empty()) {
         cout << "No integers input" << '\n';
         return -1;
     }
-
+    
+    //Check if the vector only has one element.
     else if (vec.size() == 1) {
         cout << "Only one element exists" << '\n';
         cout << "It is " << vec[0] << '\n';
     }
 
+    //Print the sum of each pair of adjacent elements
     else {
         for (decltype(vec.size()) i = 0; i < vec.size() - 1; i++) {
             cout << "Sum of these two adjacent elements is " << vec[i] + vec[i + 1] << '\n';
