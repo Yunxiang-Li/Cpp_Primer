519e480535762617f3cca172ec6e689acb8e328d
Yunxiang Li <kitzukikokone@gmail.com>
2020-07-02 01:04:09 -0400

    Update 3.32_1.cpp

--- Chapter 3. Strings, Vectors, and Arrays/Codes/3.32_1.cpp
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.32_1.cpp
@@ -5,21 +5,26 @@ using std::cout;
 
 int main()
 {
+    //Initialize two arrays with size 10.
     constexpr size_t array_size = 10;
     int ia[array_size];
     int ib[array_size];
 
+    //Set ia's each element's value to its index.
     for (size_t ix = 0; ix < array_size; ++ix)
         ia[ix] = ix;
 
+    //Output ia's each element's value.
     for (auto& element : ia)
         cout << element << ' ';
     cout << '\n';
 
+    //Assign ia's each position's value to ib's each position.
     for (size_t i = 0; i < 10; i++) {
         ib[i] = ia[i];
     }
 
+    //Output ib's each element's value.
     for (auto& element : ib)
         cout << element << ' ';
     cout << '\n';
