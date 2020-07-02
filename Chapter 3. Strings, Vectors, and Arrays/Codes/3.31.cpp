6db163f3cbb85a23d28b5541e643dea18d28bd0d
Yunxiang Li <kitzukikokone@gmail.com>
2020-07-02 01:02:33 -0400

    Update 3.31.cpp

--- Chapter 3. Strings, Vectors, and Arrays/Codes/3.31.cpp
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.31.cpp
@@ -5,12 +5,15 @@ using std::cout;
 
 int main()
 {
+    //Initialize an 10-length array ia.
     constexpr size_t array_size = 10;
     int ia[array_size];
 
+    //Set each element's value equals to its index;
     for (size_t ix = 0; ix < array_size; ++ix)
         ia[ix] = ix;
 
+    //Output each element's value.
     for (auto& element : ia)
         cout << element << ' ';
     cout << '\n';
