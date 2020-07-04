290f6f9fd1d1cd0a61f20e04478ca5d389449aaf
Yunxiang Li <kitzukikokone@gmail.com>
2020-07-04 02:35:53 -0400

    Create 3.43.cpp

---
+++ Chapter 3. Strings, Vectors, and Arrays/Codes/3.43.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    //Initialize an array with 12 elements.
+    int ia[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    // Use range for to manage the iteration.
+    for (const int(&p)[4] : ia)
+        for (int q : p)
+            cout << q << " ";
+    cout << endl;
+
+    // Use ordinary for loop using subscripts
+    for (size_t i = 0; i != 3; ++i)
+        for (size_t j = 0; j != 4; ++j)
+            cout << ia[i][j] << " ";
+    cout << endl;
+
+    // Use pointers.
+    for (int(*p)[4] = ia; p != ia + 3; ++p)
+        for (int* q = *p; q != *p + 4; ++q)
+            cout << *q << " ";
+    cout << endl;
+
+    return 0;
+}
