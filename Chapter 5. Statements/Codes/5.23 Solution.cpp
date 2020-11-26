f57bc1f16659d98fa59a96d87729055509337add
Yunxiang Li <kitzukikokone@gmail.com>
2020-11-25 23:22:10 -0500

    Update 5.23 Solution.cpp

--- Chapter 5. Statements/Codes/5.23 Solution.cpp
+++ Chapter 5. Statements/Codes/5.23 Solution.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <stdexcept>
 
 int main() {
 
   int a, b;
 
   std::cin >> a >> b;
-
-  return a / b;
   
+  std::cout << "Result of " << a << " / " << b << " is " << a / b;
+
+  return 0;
 }
