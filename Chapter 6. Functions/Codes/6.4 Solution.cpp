e9d8ebebd9c61ed91ce7cccccacd24385d26ddef
Yunxiang Li <kitzukikokone@gmail.com>
2020-11-26 17:03:03 -0500

    Update 6.4 Solution.cpp

--- Chapter 6. Functions/Codes/6.4 Solution.cpp
+++ Chapter 6. Functions/Codes/6.4 Solution.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+int fact(int val)
+{
+  if (val == 0 || val == 1)
+    return 1;
+  return val * fact(val - 1);
+}
+
+int main()
+{
+  int input;
+
+  std::cout << "Please input an unsigned integer and you will get its factorial!" << '\n';
+
+  while(std::cin >> input) {
+    if (input < 0) {
+      std::cout << "Input integer cannot be a negative integer, please try again!" << '\n';
+      continue;
+    }
+    int res = fact(input);
+    std::cout << input << "! is " << res << std::endl;
+
+    std::cout << "You can now input another integer or quit the program!" << '\n';
+  }
+  return 0;
+}
