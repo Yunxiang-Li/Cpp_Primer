7c093aead73e59015f68e525cdb8541ced45e9e4
Yunxiang Li <kitzukikokone@gmail.com>
2020-12-01 19:19:00 -0500

    Create 6.27 Solution.cpp

---
+++ Chapter 6. Functions/Codes/6.27 Solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <initializer_list>
+
+int sum(std::initializer_list<int> initial_list) {
+
+  int res{0};
+
+  for (const int &element : initial_list) {
+    res += element;
+  }
+
+  return res;
+}
+
+int main()
+{
+  int res = sum(std::initializer_list<int>{1, 2, 3, 4, 5});
+
+  std::cout << " The sum of 1-5 is " << res;
+}
