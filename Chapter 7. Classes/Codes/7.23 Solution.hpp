391729db36232b33dd7db4379e7bd20442db764b
Yunxiang Li <kitzukikokone@gmail.com>
2020-12-15 21:35:23 -0500

    Create 7.23 Solution.hpp

---
+++ Chapter 7. Classes/Codes/7.23 Solution.hpp
@@ -0,0 +1,35 @@
+#include <string>
+
+#ifndef SCREEN_HPP
+#define SCREEN_HPP
+
+class Screen {
+ public:
+  using pos = std::string::size_type;
+
+  Screen() =default;
+  Screen(pos height, pos width, char c) : m_height(height),
+                                          m_width(width),
+                                          m_contents(height * width, c)
+  {
+  };
+
+  inline char get() const {
+    return m_contents[m_cursorPos];
+  };
+
+  inline char get(pos r, pos c) const {
+    return m_contents[r * m_width + c];
+  };
+
+  inline Screen& move(pos r, pos c);
+
+ private:
+  pos m_cursorPos{0};
+  pos m_height{0};
+  pos m_width{0};
+  std::string m_contents;
+
+};
+
+#endif
