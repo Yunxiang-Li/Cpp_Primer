510f50a901ab3f6d714b7f660fee9da611dabdbb
Yunxiang Li <kitzukikokone@gmail.com>
2020-12-19 20:38:43 -0500

    Create 7.42 Solution.hpp

---
+++ Chapter 7. Classes/Codes/7.42 Solution.hpp
@@ -0,0 +1,24 @@
+
+#include <istream>
+#ifndef EX7_42_HPP
+#define EX7_42_HPP
+
+class Tree {
+ public:
+
+  Tree(int height, int width, unsigned int branch, unsigned int age): m_height(height), m_width(width),
+                                                                      m_branch(branch), m_age(age) { }
+  Tree() : Tree(0, 0, 1, 1) {}
+
+  Tree(std::istream &in) : Tree() {
+    in >> m_height >> m_width >> m_branch >> m_age;
+  }
+
+ private:
+  int m_height{0};
+  int m_width{0};
+  unsigned int m_branch{1};
+  unsigned int m_age{1};
+};
+
+#endif
