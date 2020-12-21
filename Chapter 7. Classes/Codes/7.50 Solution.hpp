b9ace8063048238f6d18a7c8ea48131b27305821
Yunxiang Li <kitzukikokone@gmail.com>
2020-12-20 19:31:57 -0500

    Create 7.50 Solution.hpp

---
+++ Chapter 7. Classes/Codes/7.50 Solution.hpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <iostream>
+#ifndef EX7_50_HPP
+#define EX7_50_HPP
+
+class Person {
+
+  friend std::istream & read(std::istream&, Person&);
+  friend std::ostream & print(std::ostream&, const Person&);
+
+ private:
+  std::string m_name;
+  std::string m_address;
+
+ public:
+
+  Person() = default;
+  Person(const std::string& name, const std::string& address)
+      : m_name(name), m_address(address)
+  {
+  }
+
+  explicit Person(std::istream& is) {
+    read(is, *this);
+  }
+
+  const std::string& getName() const;
+  const std::string& getAddress() const;
+
+  std::istream& read(std::istream& is, Person& person)
+  {
+    is >> person.m_name >> person.m_address;
+    if (!is)
+      person = Person();
+    return is;
+  }
+
+  std::ostream& print(std::ostream& os, const Person& person)
+  {
+    os << person.m_name << " " << person.m_address;
+    return os;
+  }
+};
+
+#endif
