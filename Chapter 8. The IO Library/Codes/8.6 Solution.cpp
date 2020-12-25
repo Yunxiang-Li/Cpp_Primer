77ead5da7676509d37fa83ae5811fdd955e7ba1f
Yunxiang Li <kitzukikokone@gmail.com>
2020-12-25 16:22:10 -0500

    Update 8.6 Solution.cpp

--- Chapter 8. The IO Library/Codes/8.6 Solution.cpp
+++ Chapter 8. The IO Library/Codes/8.6 Solution.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv)
 {
-    std::ifstream input(argv[1]); // use "../data/book.txt"
+    std::ifstream input(argv[1]);
 
     Sales_data total;
     if (read(input, total)) {
