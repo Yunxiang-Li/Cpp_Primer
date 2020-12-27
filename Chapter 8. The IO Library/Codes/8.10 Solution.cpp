a908268dbf15c9d05b751a8958ecbc1a7784d8b2
Yunxiang Li <kitzukikokone@gmail.com>
2020-12-26 23:10:06 -0500

    Create 8.10 Solution.cpp

---
+++ Chapter 8. The IO Library/Codes/8.10 Solution.cpp
@@ -0,0 +1,25 @@
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+void storeEachLine(std::ifstream& inputFile) {
+  if (inputFile)
+    std::cerr << "No input file provided!\n";
+  else {
+    std::string eachLine;
+    std::vector<std::string> vec;
+    while (std::getline(inputFile, eachLine)) {
+      vec.push_back(eachLine);
+    }
+
+    for (std::string &str : vec) {
+      std::istringstream inputSstream(str);
+      std::string currStr;
+      while (inputSstream >> currStr) {
+        std::cout << "Current string is " << currStr;
+      }
+    }
+  }
+
+}
