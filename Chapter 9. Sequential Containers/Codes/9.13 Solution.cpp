717fb7bdd578db7a9a1e53c0991335f113ae83cd
Yunxiang Li <kitzukikokone@gmail.com>
2020-12-28 17:13:30 -0500

    Update 9.13 Solution.cpp

--- Chapter 9. Sequential Containers/Codes/9.13 Solution.cpp
+++ Chapter 9. Sequential Containers/Codes/9.13 Solution.cpp
@@ -3,8 +3,8 @@
 #include <list>
 
 int main() {
-  std::list<int> myList = {1, 2 , 3, 4 ,5};
-  std::vector<int> myVec = {6, 7, 8, 9, 10};
+  std::list<int> myList{1, 2 , 3, 4 ,5};
+  std::vector<int> myVec{6, 7, 8, 9, 10};
 
   std::vector<double> listVec(myList.begin(), myList.end());
   for (std::vector<double>::const_iterator iter = listVec.cbegin(); iter != listVec.cend(); ++iter)
