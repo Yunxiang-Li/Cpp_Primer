8e589c1b5ee0ca4c1cedd7b47da6a45529e1fc94
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-05 15:00:51 -0500

    Create 9.31_2 Solution.cpp

---
+++ Chapter 9. Sequential Containers/Codes/9.31_2 Solution.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <forward_list>
+
+int main() {
+  std::forward_list<int> myFList{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  auto prev = myFList.before_begin(), curr = myFList.begin();
+
+  while (curr != myFList.end()) {
+    if (*curr % 2) {
+      prev = myFList.insert_after(prev, *curr);
+      ++curr;
+      ++prev;
+    }
+    else {
+      curr = myFList.erase_after(prev);
+    }
+  }
+
+  for (const int element : myFList) {
+    std::cout << element << ' ';
+  }
+}
