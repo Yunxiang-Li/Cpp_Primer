24817dd86ab7b802d509bb08fdec5f14ad2ad112
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-06 23:06:39 -0500

    Update 9.43 Solution.cpp

--- Chapter 9. Sequential Containers/Codes/9.43 Solution.cpp
+++ Chapter 9. Sequential Containers/Codes/9.43 Solution.cpp
@@ -2,18 +2,18 @@
 #include <string>
 
 void replaceOldWithNew(std::string& oriStr, const std::string& oldValStr, const std::string& newValStr) {
- for (auto iter = oriStr.begin(); iter != oriStr.end(); ++iter) {
-   std::string temp(iter, iter + oldValStr.size());
-   if (temp == oldValStr) {
+  for (auto iter = oriStr.begin(); iter != oriStr.end(); ++iter) {
+    std::string temp(iter, iter + oldValStr.size());
+    if (temp == oldValStr) {
       iter = oriStr.erase(iter, iter + oldValStr.size());
       iter = oriStr.insert(iter, newValStr.cbegin(), newValStr.cend());
-   }
- }
+    }
+  }
 }
 
 int main()
 {
-  std::string s{"tho thru apt vs"};
+  std::string s{"tho thru apt vs vs"};
   std::cout << "Original string is: " << s << '\n';
 
   std::cout << "Try to replace a non-exist value" << '\n';
@@ -32,9 +32,11 @@ int main()
   replaceOldWithNew(s, "apt", "apartment");
   std::cout << "Current string is: " << s << '\n';
 
-  std::cout << "Replace fourth value" << '\n';
+  std::cout << "Replace last two values" << '\n';
   replaceOldWithNew(s, "vs", "versus");
   std::cout << "Current string is: " << s << '\n';
 
   return 0;
 }
+
+
