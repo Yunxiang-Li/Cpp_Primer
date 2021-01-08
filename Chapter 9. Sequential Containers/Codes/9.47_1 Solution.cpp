f5c513477c74d7695c5b9ed3d6d182ce67a9b59e
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-08 16:29:13 -0500

    Create 9.47_1 Solution.cpp

---
+++ Chapter 9. Sequential Containers/Codes/9.47_1 Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string str{"ab2c3d7R4E6"};
+  std::string numStr{"0123456789"};
+  std::string alphabetStr{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+
+  std::cout << "First find each numeric character" << '\n';
+  for (std::string::size_type pos = 0; (pos = str.find_first_of(numStr, pos)) != std::string::npos; ++pos)
+    std::cout << "Find one numeric character: " << str[pos] << '\n';
+
+  std::cout << "Next find each alphabetic character" << '\n';
+  for (std::string::size_type pos = 0; (pos = str.find_first_of(alphabetStr, pos)) != std::string::npos; ++pos)
+    std::cout << "Find one alphabetic character: " << str[pos] << '\n';
+
+  return 0;
+}
