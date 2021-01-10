a7ea52253ec389f688554d2bc798906b4be80763
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-09 19:59:23 -0500

    Update 10.5 Solution.cpp

--- Chapter 10. Generic Algorithms/Codes/10.5 Solution.cpp
+++ Chapter 10. Generic Algorithms/Codes/10.5 Solution.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <cstring>
 
 int main() {
   char cArrayOne[]{"abcdefg"};
   char cArrayTwo[]{"abcdefg"};
   std::vector<char*> roster1{cArrayOne};
   std::list<char*> roster2{cArrayTwo};
-  int res = std::equal(roster1.cbegin(), roster1.cend(), roster2.cbegin());
-  std::cout << res;
+  bool res = std::equal(roster1.cbegin(), roster1.cend(), roster2.cbegin());
+  std::cout << "The result of std::equal function is " << std::boolalpha << res << '\n';
+
+  int res2 = std::strcmp(cArrayOne, cArrayOne);
+  std::cout << "The result of std::strcmp function is " << res2;
   return 0;
 }
