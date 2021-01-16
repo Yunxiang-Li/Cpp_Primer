e144e3d59e5db74c6470184021d994fe1e8b56ca
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-15 23:36:47 -0500

    Create 10.22 Solution.cpp

---
+++ Chapter 10. Generic Algorithms/Codes/10.22 Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+bool strSizeLessThanOrEqualTo(const std::string& str, std::string::size_type sz) {
+  return str.size() <= sz;
+}
+
+int main()
+{
+  std::vector<std::string> vec{"dog", "in", "deliver", "sword", "clever", "conversation", "nevertheless"};
+
+  std::cout << "Altogether there are " <<
+  std::count_if(vec.cbegin(), vec.cend(), std::bind(strSizeLessThanOrEqualTo, std::placeholders::_1, 6))
+  << " strings which size are less than or equal to 6!";
+  return 0;
+}
