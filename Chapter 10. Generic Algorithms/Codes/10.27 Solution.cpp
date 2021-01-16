e6caabb9361862d207c17b4320348b1c68f33bdf
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-16 17:15:19 -0500

    Update 10.27 Solution.cpp

--- Chapter 10. Generic Algorithms/Codes/10.27 Solution.cpp
+++ Chapter 10. Generic Algorithms/Codes/10.27 Solution.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <list>
 
+int main()
+{
+
+  std::vector<int> vec{0, 0, 1, 1, 2, 3, 4, 5, 5, 5, 5};
+  std::list<int> myList;
+
+  std::unique_copy(vec.cbegin(), vec.cend(), std::back_inserter(myList));
+
+  for (const int element : myList)
+    std::cout << element << ' ';
+
+  return 0;
+}
