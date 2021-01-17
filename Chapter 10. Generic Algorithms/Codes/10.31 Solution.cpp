1aeb59bd8b77e2bee5937aae5aded1709cbaef2b
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-17 16:35:44 -0500

    Create 10.31 Solution.cpp

---
+++ Chapter 10. Generic Algorithms/Codes/10.31 Solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+
+int main()
+{
+  // Get int sequences from the standard input.
+  std::istream_iterator<int> istream_iterator(std::cin), istream_endIter;
+  // Copy int sequences from standard input to the vector and sort all integers.
+  std::vector<int> vec(istream_iterator, istream_endIter);
+  std::sort(vec.begin(), vec.end());
+
+  // Output the vector's elements.
+  std::ostream_iterator<int> ostream_iterator(std::cout, " ");
+  std::unique_copy(vec.cbegin(), vec.cend(), ostream_iterator);
+
+  return 0;
+}
