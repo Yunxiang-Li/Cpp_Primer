51b695b40bc323bcc538ffd4a992f9bda6959738
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-22 20:10:23 -0500

    Update and rename 1.cpp to 11.3 Solution.cpp

---
+++ Chapter 11. Associative Containers/Codes/11.3 Solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main()
+{
+  std::map<std::string, size_t> myMap;
+  std::string word;
+  while (std::cin >> word) {
+    ++myMap[word];
+  }
+  for (const std::pair<std::string, size_t> eachPair : myMap)
+    std::cout << eachPair.first << " appears " << eachPair.second <<
+    ((eachPair.second >= 1) ? " times" : " time") << '\n';
+  std::cout << std::endl;
+}
