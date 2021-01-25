80d0a55369ed03e3751c2855c74dab0711223ed2
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-25 17:31:23 -0500

    Update 11.12 Solution.cpp

--- Chapter 11. Associative Containers/Codes/11.12 Solution.cpp
+++ Chapter 11. Associative Containers/Codes/11.12 Solution.cpp
@@ -12,7 +12,7 @@ int main() {
     vec.emplace_back(str, i);
   }
 
-  for (const auto eachPair : vec)
+  for (const auto &eachPair : vec)
     std::cout << "Current pair's key is " << eachPair.first << " ,value is " << eachPair.second << '\n';
 
   return 0;
