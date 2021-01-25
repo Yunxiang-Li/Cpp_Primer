891bc9e6c40031e2e2ea5c71cc1db0ed3b720107
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-25 17:20:29 -0500

    Create 11.12 Solution.cpp

---
+++ Chapter 11. Associative Containers/Codes/11.12 Solution.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+  std::string str;
+  int i;
+  std::vector<std::pair<std::string, int>> vec;
+  std::cout << "Please input pairs of strings and integers\n";
+  while (std::cin >> str >> i) {
+    vec.emplace_back(str, i);
+  }
+
+  for (const auto eachPair : vec)
+    std::cout << "Current pair's key is " << eachPair.first << " ,value is " << eachPair.second << '\n';
+
+  return 0;
+}
