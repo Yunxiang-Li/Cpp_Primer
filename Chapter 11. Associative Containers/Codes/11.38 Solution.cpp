4e6e4de15f742dd5969ab625dbb0a0d19859b33c
Yunxiang Li <kitzukikokone@gmail.com>
2021-01-28 18:41:54 -0500

    Create 11.38 Solution.cpp

---
+++ Chapter 11. Associative Containers/Codes/11.38 Solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+int main()
+{
+  std::unordered_map<std::string, size_t> myMap;
+  std::string word;
+  while (std::cin >> word) {
+    ++myMap[word];
+  }
+  for (const std::pair<std::string, size_t> eachPair : myMap)
+    std::cout << eachPair.first << " appears " << eachPair.second <<
+              ((eachPair.second >= 1) ? " times" : " time") << '\n';
+  std::cout << std::endl;
+
+  return 0;
+}
