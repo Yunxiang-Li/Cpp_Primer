009fed0c9f47c0181ebacde78bac5738d992ed8c
Yunxiang Li <kitzukikokone@gmail.com>
2021-02-08 19:44:55 -0500

    Update 12.16 Solution.cpp

--- Chapter 12. Dynamic Memory/Codes/12.16 Solution.cpp
+++ Chapter 12. Dynamic Memory/Codes/12.16 Solution.cpp
@@ -5,8 +5,10 @@
 int main()
 {
     std::unique_ptr<std::string> unique_ptrOne(new std::string("unique"));
-    std::unique_ptr<std::string> unique_ptrTwo(unique_ptrOne);
-    std::unique_ptr<std::string> unique_ptrThree = unique_ptrOne;
+    //std::unique_ptr<std::string> unique_ptrTwo(unique_ptrOne);
+    //std::unique_ptr<std::string> unique_ptrThree = unique_ptrOne;
 
+    std::cout << *unique_ptrOne << '\n';
+    unique_ptrOne.reset();
     return 0;
 }
