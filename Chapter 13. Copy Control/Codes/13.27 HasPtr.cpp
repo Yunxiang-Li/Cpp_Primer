653ed1569e323017b6776fd513e60ec62118c75b
Yunxiang Li <kitzukikokone@gmail.com>
2021-05-12 01:04:19 -0400

    Create 13.27 HasPtr.cpp

---
+++ Chapter 13. Copy Control/Codes/13.27 HasPtr.cpp
@@ -0,0 +1,25 @@
+#include "HasPtr.hpp"
+
+HasPtr& HasPtr::operator=(const HasPtr& hp)
+{
+    ++(*hp.m_countPtr);
+
+    if (--m_countPtr == 0) {
+        delete this -> ps;
+        delete m_countPtr;
+    }
+
+    this -> ps = hp.ps;
+    this -> i = hp.i;
+    m_countPtr = hp.m_countPtr;
+
+    return *this;
+}
+
+HasPtr::~HasPtr() {
+
+    if (--m_countPtr == 0) {
+        delete this -> ps;
+        delete m_countPtr;
+    }
+}
