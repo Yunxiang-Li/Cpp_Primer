34767254432b87dd048f0798ab758bcc93e843f2
Yunxiang Li <kitzukikokone@gmail.com>
2021-05-14 14:11:12 -0400

    Create 13.30 HasPtr.hpp

---
+++ Chapter 13. Copy Control/Codes/13.30 HasPtr.hpp
@@ -0,0 +1,35 @@
+#ifndef HASPTR_HPP
+#define HASPTR_HPP
+
+#include <string>
+#include <iostream>
+
+class HasPtr
+{
+  friend void swap(HasPtr&, HasPtr&);
+
+ public:
+  HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0), m_countPtr(new std::size_t(1)) { }
+
+  HasPtr(const HasPtr& hp) : ps(hp.ps), i(hp.i), m_countPtr(hp.m_countPtr) { ++(*m_countPtr);}
+
+  HasPtr& operator=(const HasPtr& hp);
+
+  ~HasPtr();
+
+ private:
+  std::string *ps;
+  int    i;
+  std::size_t* m_countPtr;
+
+};
+
+inline void swap(HasPtr& HasPtrOne, HasPtr& HasPtrTwo) {
+    std::swap(HasPtrOne.i, HasPtrTwo.i);
+    std::swap(HasPtrOne.ps, HasPtrTwo.ps);
+    std::swap(HasPtrOne.m_countPtr, HasPtrTwo.m_countPtr);
+
+    std::cout << "swap finished!" << '\n';
+}
+
+#endif
