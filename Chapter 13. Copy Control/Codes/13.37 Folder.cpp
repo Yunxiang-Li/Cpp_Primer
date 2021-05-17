71e2ffaed3d9e575af0f38e101eb96b203e06aac
Yunxiang Li <kitzukikokone@gmail.com>
2021-05-17 01:44:16 -0400

    Create 13.37 Folder.cpp

---
+++ Chapter 13. Copy Control/Codes/13.37 Folder.cpp
@@ -0,0 +1,54 @@
+#include "Folder.hpp"
+#include "Message.hpp"
+#include <iostream>
+
+Folder::Folder(const Folder& folder) : m_msgs(folder.m_msgs){
+    m_add_to_Messages(*this);
+}
+
+Folder& Folder::operator=(const Folder& folder) {
+    m_remove_from_Messages();
+    m_msgs = folder.m_msgs;
+    m_add_to_Messages(*this);
+
+    return *this;
+}
+
+Folder::~Folder() {
+    m_remove_from_Messages();
+}
+
+void Folder::m_add_to_Messages(const Folder& folder) {
+    for (auto msgPtr : folder.m_msgs)
+        msgPtr -> m_add_folder(this);
+}
+
+void Folder::m_remove_from_Messages() {
+    for (auto msgPtr : m_msgs)
+        msgPtr -> m_remove_folder(this);
+}
+
+void Folder::addMsg(Message* msgPtr) {
+    m_msgs.insert(msgPtr);
+}
+
+void Folder::remMsg(Message* msgPtr) {
+    m_msgs.erase(msgPtr);
+}
+
+void Folder::m_printContent() {
+    std::cout << '\n' << '\n' << "Folder content start: " << '\n';
+    for (auto msgPtr : m_msgs)
+        msgPtr -> m_printContent();
+    std::cout << '\n' << "Folder content end!" << '\n' << '\n';
+}
+
+void swap(Folder& folderOne, Folder& folderTwo) {
+    folderOne.m_remove_from_Messages();
+    folderTwo.m_remove_from_Messages();
+
+    std::swap(folderOne.m_msgs, folderTwo.m_msgs);
+
+    folderOne.m_add_to_Messages(folderOne);
+    folderTwo.m_add_to_Messages(folderTwo);
+}
