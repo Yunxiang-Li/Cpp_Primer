cbf3f07239f9dbf5ea4da5298b3888b88802864e
Yunxiang Li <kitzukikokone@gmail.com>
2021-05-21 04:52:00 -0400

    Create 10.39 StrVec.hpp

---
+++ Chapter 13. Copy Control/Codes/10.39 StrVec.hpp
@@ -0,0 +1,39 @@
+#ifndef STRVEC_HPP
+#define STRVEC_HPP
+
+#include <string>
+
+class StrVec {
+ public:
+  StrVec() : m_elements(nullptr), m_first_free(nullptr), m_cap(nullptr) {}
+  StrVec(const StrVec&);
+  StrVec& operator=(const StrVec&);
+  ~StrVec();
+
+  void m_push_back(const std::string&);
+  size_t m_size() const;
+  size_t m_capacity() const;
+  std::string* m_begin() const;
+  std::string* m_end() const;
+
+  void m_reserve(size_t newCap);
+  void m_resize(size_t newSize);
+  void m_resize(size_t newSize, const std::string&);
+
+
+ private:
+    std::pair<std::string*, std::string*> m_alloc_n_copy(const std::string*, const std::string*);
+    void m_free();
+    void m_chk_n_alloc();
+    void m_reallocate();
+    void m_alloc_n_move(size_t newCap);
+
+    std::string* m_elements;
+    std::string* m_first_free;
+    std::string* m_cap;
+    static std::allocator<std::string> alloc;
+};
+
+std::allocator<std::string>StrVec::alloc;
+
+#endif
