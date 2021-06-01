b5818c91a3bbfde500b136ffd3a788520833e78b
Yunxiang Li <kitzukikokone@gmail.com>
2021-06-01 18:14:17 -0400

    Create 14.16 String.hpp

---
+++ Chapter 14. Overloaded Operations and Conversions/Codes/14.16 String.hpp
@@ -0,0 +1,42 @@
+#ifndef STRING_HPP
+#define STRING_HPP
+
+#include <cstdlib>
+#include <utility>
+#include <memory>
+
+class String {
+
+  friend std::ostream& operator<<(std::ostream&, const String&);
+
+  friend bool operator==(const String&, const String&);
+  friend bool operator!=(const String&, const String&);
+
+ public:
+    String() : String("") {}
+    String(const char*);
+    String(const String&);
+    String& operator=(const String&);
+    String(String&&) noexcept;
+    String& operator=(String&&) noexcept;
+    ~String();
+
+    char* m_begin() const;
+    char* m_end() const;
+
+    const char* m_c_str() const;
+    size_t m_size() const;
+    size_t m_length() const;
+
+ private:
+    std::pair<char*, char*> m_alloc_n_copy(const char*, const char*);
+    void m_range_initialize(const char*, const char*);
+    void m_free();
+
+    char* m_elements;
+    char* m_first_free;
+
+    static std::allocator<char> alloc;
+};
+
+#endif
