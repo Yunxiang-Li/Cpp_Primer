feafbba87fa793db73c4da5f09b1af1607b9cb6e
Yunxiang Li <kitzukikokone@gmail.com>
2021-06-21 02:29:32 -0400

    Create 15.11 Bulk_quote.hpp

---
+++ Chapter 15. Object-Oriented Programming/Codes/15.11 Bulk_quote.hpp
@@ -0,0 +1,20 @@
+#ifndef BULK_QUOTE_HPP
+#define BULK_QUOTE_HPP
+
+#include "Quote.hpp"
+
+class Bulk_quote : public Quote {
+ public:
+  Bulk_quote() = default;
+  Bulk_quote(const std::string& bookNo, double price, size_t qty, double discount) : Quote(bookNo, price), m_qty(qty),
+  m_discount(discount) {}
+
+  virtual double net_price(size_t n) const override;
+  virtual void debug() const override;
+
+ protected:
+  size_t m_qty{0};
+  double m_discount{0.0};
+};
+
+#endif
