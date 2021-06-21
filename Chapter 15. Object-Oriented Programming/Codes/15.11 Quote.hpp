e41fcbfc0818d2e7108f2ba4ee16ae9ad9091d66
Yunxiang Li <kitzukikokone@gmail.com>
2021-06-21 02:29:05 -0400

    Create 15.11 Quote.hpp

---
+++ Chapter 15. Object-Oriented Programming/Codes/15.11 Quote.hpp
@@ -0,0 +1,32 @@
+#ifndef QUOTE_HPP
+#define QUOTE_HPP
+
+#include <iostream>
+#include <string>
+
+class Quote {
+ public:
+  Quote() = default;
+  Quote(const std::string& bookNo, double price) : mBookNo(bookNo), mPrice(price) {}
+
+  std::string isbn() const;
+  virtual double net_price(size_t n) const;
+  virtual void debug() const;
+
+  virtual ~Quote() = default;
+
+ private:
+  std::string mBookNo;
+
+ protected:
+  double mPrice;
+};
+
+static double printTotal(std::ostream& os, const Quote& quote, size_t n) {
+    double res = quote.net_price(n);
+    os << "ISBN: " << quote.isbn() << " # sold: " << n << " total due: " << res << '\n';
+
+    return res;
+}
+
+#endif
