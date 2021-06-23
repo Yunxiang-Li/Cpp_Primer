8a172b46e8a92420f0906762395843917ddded84
Yunxiang Li <kitzukikokone@gmail.com>
2021-06-23 02:52:54 -0400

    Update 15.7 Limit_quote.hpp

--- Chapter 15. Object-Oriented Programming/Codes/15.7 Limit_quote.hpp
+++ Chapter 15. Object-Oriented Programming/Codes/15.7 Limit_quote.hpp
@@ -1,19 +1,20 @@
 #ifndef LIMIT_QUOTE_HPP
 #define LIMIT_QUOTE_HPP
 
-#include "Quote.hpp"
+#include "Disc_quote.hpp"
 
-class Limit_quote : public Quote {
+class Limit_quote : public Disc_quote {
  public:
   Limit_quote() = default;
   Limit_quote(const std::string& bookNo, double price, size_t max, double discount) :
-      Quote(bookNo, price), m_max(max), m_discount(discount) {}
+      Disc_quote(bookNo, price, max, discount), m_max(max) {}
 
   virtual double net_price(size_t n) const override;
+  virtual void debug() const override;
 
  protected:
-  size_t m_max;
   size_t m_discount;
+  size_t m_max;
 };
 
 #endif
